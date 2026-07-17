bdcc22d49565ec8ef9172d11d8724031ce2c620a
Kin Basco <freepeace13@gmail.com>
2026-07-18 04:32:39 +0800

    routes: remove the dead /api/crawler/callback endpoint and its generated wayfinder output

--- apps/web/routes/api.php
+++ apps/web/routes/api.php
@@ -1,9 +1,1 @@
 <?php
-
-use App\Http\Controllers\Api\CrawlerCallbackController;
-use App\Http\Middleware\CrawlerMiddleware;
-use Illuminate\Support\Facades\Route;
-
-Route::post('/crawler/callback', CrawlerCallbackController::class)
-    ->middleware(CrawlerMiddleware::class)
-    ->name('api.crawler.callback');
